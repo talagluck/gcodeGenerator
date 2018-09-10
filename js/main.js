9ac50289e3dc2002103c74aec5be4a7fc90c38be
babyshoes <dianecai@gmail.com>
2018-09-10 10:15:37 -0400

    lil clean-up

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 const canvas = document.getElementById("canvas");
-const clock = new THREE.Clock();
 const sceneManager = new SceneManager(canvas);
-const mouse = new THREE.Vector2();
-sceneManager.mouse = mouse;
-
-const minX = 4;
-const maxX = 50
-const minY = -51;
-const maxY = 51;
 
 bindEventListeners();
 render();
@@ -36,7 +28,6 @@ function bindEventListeners() {
 function onMouseClick( event ) {
 	event.preventDefault();
 
-	const elapsedTime = clock.getElapsedTime();
 	// calculate mouse position in normalized device coordinates
 	// (-1 to +1) for both components
 	const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
@@ -56,6 +47,7 @@ function onMouseClick( event ) {
 		let planeIntersects = sceneManager.raycaster.intersectObject(sceneManager.plane.mesh);
 		sceneManager.offset = planeIntersects[0].point.sub(sceneManager.plane.mesh.position);
 	}
+
 	//grid point intersection
 	this.gridPointIntersects = sceneManager.raycaster.intersectObjects(sceneManager.gridPointList.map(obj => obj.meshAttr()));
 	if (gridPointIntersects.length > 0) {
@@ -73,6 +65,11 @@ function onMouseClick( event ) {
 function onMouseMove ( event ) {
 	event.preventDefault();
 
+	const minX = 4;
+	const maxX = 50
+	const minY = -51;
+	const maxY = 51;
+
 	const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
 	const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
 
@@ -80,43 +77,36 @@ function onMouseMove ( event ) {
 	vector.unproject(sceneManager.camera)
 	
 	const currentAnchorPtMesh = sceneManager.selection;
-
 	sceneManager.raycaster.set(sceneManager.camera.position, vector.sub(sceneManager.camera.position).normalize());
 
 	if (currentAnchorPtMesh){
 		const intersects = sceneManager.raycaster.intersectObjects([sceneManager.plane.mesh]);
 		sceneManager.selection.position.copy(intersects[0].point.sub(sceneManager.offset));
 
-		var i = sceneManager.anchorPointList.map(obj => obj.mesh.uuid).indexOf(currentAnchorPtMesh.uuid);
-		var top = i == 0 ? maxY : sceneManager.anchorPointList[i - 1].mesh.position.y;
-		var bottom = i == sceneManager.anchorPointList.length - 1 ? minY : sceneManager.anchorPointList[i + 1].mesh.position.y;
-		
-		if (sceneManager.anchorPointList[i].mesh.position.x < minX) {
-			sceneManager.anchorPointList[i].mesh.position.x = minX;
-		}
-		if (sceneManager.anchorPointList[i].mesh.position.x > maxX) {
-			sceneManager.anchorPointList[i].mesh.position.x = maxX;
-		}
-		if (sceneManager.anchorPointList[i].mesh.position.y >= top - 1) {
-			sceneManager.anchorPointList[i].mesh.position.y = top - 1;
-		}
-		if (sceneManager.anchorPointList[i].mesh.position.y <= bottom + 1) {
-			sceneManager.anchorPointList[i].mesh.position.y = bottom + 1;
-		}
+		const i = sceneManager.anchorPointList.map(obj => obj.mesh.uuid).indexOf(currentAnchorPtMesh.uuid);
+		const currentAnchorPoint = sceneManager.anchorPointList[i].mesh;
+		const top = i == 0 ? maxY : sceneManager.anchorPointList[i - 1].mesh.position.y;
+		const bottom = i == sceneManager.anchorPointList.length - 1 ? minY : sceneManager.anchorPointList[i + 1].mesh.position.y;
 
-		let gridPointsToHide = [];
-		anchorPointList.forEach(anc => 
-			{let gridAnchorNear = sceneManager.gridPointList.filter(obj => Math.abs(obj.meshAttr().position.y - anc.mesh.position.y) < 2)
-			gridPointsToHide.push(gridAnchorNear[0])}
-		)	
-		if(gridPointsToHide.length>=1){
-			gridPointsToHide.map(obj=> { if(obj) { obj.hideGridPoint() } } )
-		}
+		if (currentAnchorPoint.position.x < minX) { currentAnchorPoint.position.x = minX; }
+		if (currentAnchorPoint.position.x > maxX) { currentAnchorPoint.position.x = maxX; }
+		if (currentAnchorPoint.position.y >= top - 1) { currentAnchorPoint.position.y = top - 1; }
+		if (currentAnchorPoint.position.y <= bottom + 1) { currentAnchorPoint.position.y = bottom + 1; }
+
+		const gridPointsToHide = []
+		anchorPointList.forEach(anc => {
+			let gridAnchorNear = sceneManager.gridPointList.filter(obj => Math.abs(obj.meshAttr().position.y - anc.mesh.position.y) < 2);
+			if (gridAnchorNear[0]) { 
+				gridAnchorNear[0].hideGridPoint();
+				gridPointsToHide.push(gridAnchorNear[0]);	
+			};
+		})	
 
 		let gridPointsToShow = sceneManager.gridPointList.filter(obj => -1 === gridPointsToHide.indexOf(obj));
 		gridPointsToShow.map(obj => obj.showGridPoint());
 		eventBus.post("buildNewLathe");
 		eventBus.post("buildNewSpiral");
+		
 	} else {
 		const intersects = sceneManager.raycaster.intersectObjects(sceneManager.anchorPointList.map(obj => obj.mesh));
 		if(intersects.length > 0){
